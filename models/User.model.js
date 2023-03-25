d2c8f5cadcf2eef17c379505556442ed7437755e
Juansecode <juan.sebastianri28@gmail.com>
2023-03-25 16:06:37 -0500

    IDRU-10 Creacion del esquema con los datos que se almacenaran de un usuario

--- models/User.model.js
+++ models/User.model.js
@@ -1,8 +1,56 @@
 import {Schema, model} from 'mongoose';
 
+/* Creating a schema for the user. */
 const userSchema = new Schema({
-    // Atributos que contendra el usuario
-
+    email: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type:String,
+        required: true
+    },
+    name: {
+        type:String,
+        required: true
+    },
+    job: {
+        type:String,
+        required: true
+    },
+    profile: {
+        type:String,
+        required: true
+    },
+    contact: {
+        phone: String,
+        web: String,
+    }, 
+    education: [{
+        degree: String,
+        start: Date, 
+        end: Date, 
+        location: String, 
+        college: String, 
+        description: String,
+    }],
+    experience: [{
+        job: String,
+        start: Date, 
+        end: Date, 
+        location: String, 
+        company: String, 
+        description: String,
+    }],
+    skills: [{
+        skill: String, 
+        rating: Number,
+    }],
+    languages: [{
+        language: String, 
+        rating: Number,
+    }],
 });
 
 const UserModel = model('User', userSchema);

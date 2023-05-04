248f3483c5dd25a00afec1f81612d75bc93eed88
Juansecode <juan.sebastianri28@gmail.com>
2023-05-04 15:54:41 -0500

    Uso de las rutas definidas

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import {PORT, app} from "./config/server.js";
+import {router as userRouter} from "./routes/user.routes.js";
+
+app.use('/user', userRouter);
 
 app.listen(PORT, ()=>{
     console.log(`Servidor en el puerto: ${PORT}`);

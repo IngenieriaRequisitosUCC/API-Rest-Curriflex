84edb33945f48b2befab2b04080cb11ec77e0f04
Sarah-mp <kirapra2017@gmail.com>
2023-05-17 19:33:22 -0500

    validate email and password

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import {PORT, app} from "./config/server.js";
+import { validateEmail, validatePassword } from "./utils/utils.js";
 
 app.listen(PORT, ()=>{
     console.log(`Servidor en el puerto: ${PORT}`);
+    console.log(`el email es: ${validateEmail("prueba@gmail.com")}`);
+    console.log(`la password es: ${validatePassword("Lasmdso29.")}`);
 });
---
+++ utils/utils.js
@@ -0,0 +1,21 @@
+function validateEmail(email){
+    
+    var regexExpression =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+    if( regexExpression.test(email) ){
+        return true;
+    }
+    return false;
+} 
+
+function validatePassword(password){
+    
+    var regexExpression =  /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+    if( regexExpression.test(password) ){
+        return true;
+    }
+    return false;
+} 
+
+export {validatePassword, validateEmail}
